273c84200171ed3f237babb226c9110fe15a318b
Jonathan Linat <jonathan.linat@gmail.com>
2024-06-26 23:46:24 -0400

    chore(ADHOC): initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/jonathan/Projects/Personal/jonathanlinat/dod13community-website/node_modules/.pnpm/infima@0.2.0-alpha.43/node_modules/infima/dist/css/default/default.css"),
+  require("/home/jonathan/Projects/Personal/jonathanlinat/dod13community-website/node_modules/.pnpm/@docusaurus+theme-classic@3.4.0_@types+react@18.3.3_react-dom@18.3.1_react@18.3.1_typescript@5.5.2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/jonathan/Projects/Personal/jonathanlinat/dod13community-website/node_modules/.pnpm/@docusaurus+theme-classic@3.4.0_@types+react@18.3.3_react-dom@18.3.1_react@18.3.1_typescript@5.5.2/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/jonathan/Projects/Personal/jonathanlinat/dod13community-website/src/css/custom.css"),
+];
